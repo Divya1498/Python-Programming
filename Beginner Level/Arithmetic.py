4f052510e49c4b94dfb370c515c05ec2d7dce085
Divya1498 <36623851+Divya1498@users.noreply.github.com>
2018-02-20 11:30:00 +0530

    Update Arithmetic.py

--- Beginner Level/Arithmetic.py
+++ Beginner Level/Arithmetic.py
@@ -1,12 +1,12 @@
 a=20
 b=5
 c=int(a)+int(b)
-Print('Addition of {0} and {1} is {2}'.format(a,b,c))
+print('Addition of {0} and {1} is {2}'.format(a,b,c))
 d=4
 e=2
 f=int(d)/int(e)
-Print('Division of {0} and {1} is {2}'.format(d,e,f))
+print('Division of {0} and {1} is {2}'.format(d,e,f))
 g=5
 h=8
 i=int(g)*int(h)
-Print('Multiplication of {0} and {1} is {2}'.format(g,h,i))
+print('Multiplication of {0} and {1} is {2}'.format(g,h,i))
