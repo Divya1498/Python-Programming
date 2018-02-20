d8d5f60afa30b86848b5a4b1528c4ab317bbe417
Divya1498 <36623851+Divya1498@users.noreply.github.com>
2018-02-20 12:01:37 +0530

    Create Basic operation.py

---
+++ Beginner Level/Basic operation.py
@@ -0,0 +1,17 @@
+a=20
+b=10
+c=int(a)+int(b)
+print 'addition',c
+d=int(a)-int(b)
+print 'subtraction',d
+e=int(a)*int(b)
+print 'multiplication',e
+f=21
+g=5
+h=int(f)/int(g)
+print 'Division',h
+i=int(f)%int(g)
+print 'remainder',i
+k=2
+j=f**k
+print 'square of number',j
