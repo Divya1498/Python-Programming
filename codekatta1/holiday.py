5cfc1f2b0501598d51aa1460151a9994ad9d9ebb
Divya1498 <36623851+Divya1498@users.noreply.github.com>
2018-06-04 13:08:56 +0530

    Create holiday.py

---
+++ codekatta1/holiday.py
@@ -0,0 +1,5 @@
+a=raw_input()
+if(a=='SUNDAY' or a=='sunday' or a=='SATURDAY' or a=='saturday'):
+	print("yes...holiday")
+elif(a=='MONDAY' or a=='monday' or a=='tuesday' or a=='TUESDAY' or a=='wednesday' or a=='WEDNESDAY' or a=='thursday' or a=='THURSDAY' or a=='friday' or a=='FRIDAY'):
+	print("no...not a holiday")
